a8eec3a0b451ae31195145e3f4210eff6e3f0b18
LauraNicole449 <lauranicolegarcia449@gmail.com>
2023-12-18 18:03:02 +0100

    calculator semi-finished

---
+++ README.md
@@ -0,0 +1,2 @@
+ calculator created with html, css and javascript who can change themes
+
--- calculadora.js
+++ calculadora.js
@@ -24,36 +24,63 @@ let option = 2;
 
 function changeTheme(){
   let body = document.getElementById("body");
+  let themeChangeBack = document.getElementById("theme-change");
   let buttonTheme = document.getElementById("theme-change-button");
-  let calBack = document.getElementById("calc-background"); 
+  let resultDisplay = document.getElementById("result-display");
+  let calBack = document.getElementById("calc-background");
+  let buttons = document.getElementsByClass("buttons");
+  let deleteButton = document.getElementById("delete");
+  let resetButton = document.getElementById("reset");
+  let resultButton = document.getElementByIf("result");
   
   
   switch (option) {
     case 1:
-      if (button) {
-        button.style.left = "0px";
+      if (buttonTheme) {
+        buttonTheme.style.left = "0px";
         body.style.backgroundColor = "#3b4664";
+        themeChangeBack.style.backgroundColor = "#252d44";
         buttonTheme.style.backgroundColor = "#d73b34";
-        calBack.style.backgroundColor = "";
+        resultDisplay.style.color = "#181f32"
+        calBack.style.backgroundColor = "#252d44";
+        buttons.style.backgroundColor = "#252d44";
+        deleteButton.style.backgroundColor = "#181f32";
+        resetButton.style.backgroundColor = "#181f32";
+        resultButton.style.backgroundColor = "#181f32";
         option = 2; 
       } else {
         return;
       }
       break;
     case 2:
-      if (button) {
-        button.style.left = "28px";
-        body.style.backgroundColor = "#e6e6e6";
-        calBack.style.backgroundColor = "red";
+      if (buttonTheme) {
+        buttonTheme.style.left = "28px";
+        body.style.backgroundColor = "#3b4664";
+        themeChangeBack.style.backgroundColor = "#252d44";
+        buttonTheme.style.backgroundColor = "#d73b34";
+        resultDisplay.style.color = "#181f32"
+        calBack.style.backgroundColor = "#252d44";
+        buttons.style.backgroundColor = "#252d44";
+        deleteButton.style.backgroundColor = "#181f32";
+        resetButton.style.backgroundColor = "#181f32";
+        resultButton.style.backgroundColor = "#181f32";
         option = 3; 
       } else {
         return;
       }
       break;
     case 3:
-      if (button) {
-        button.style.left = "55px";
-        calBack.style.backgroundColor = "white";
+      if (buttonTheme) {
+        buttonTheme.style.left = "55px";
+        body.style.backgroundColor = "#3b4664";
+        themeChangeBack.style.backgroundColor = "#252d44";
+        buttonTheme.style.backgroundColor = "#d73b34";
+        resultDisplay.style.color = "#181f32"
+        calBack.style.backgroundColor = "#252d44";
+        buttons.style.backgroundColor = "#252d44";
+        deleteButton.style.backgroundColor = "#181f32";
+        resetButton.style.backgroundColor = "#181f32";
+        resultButton.style.backgroundColor = "#181f32";
         option = 1; 
       } else {
         return;
